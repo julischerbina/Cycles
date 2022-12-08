cfe60c62eab2f09357e616b10665ceae96fc722f
jul <julia_sky@mail.ru>
2022-12-08 09:48:49 +0300

    Создание проекта

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        for(int i = 1; i <= 10; i = i + 1) {
+            System.out.println(i); // Пишем код для задачи 1
+        }
+    }
+    public static void task2 () {
+        System.out.println("Задача 2");
+        for(int i = 1; i <= 10; i = i + 1) {
+            System.out.println("i"); // Пишем код для задачи 1
+        }
+    }
+
+}
